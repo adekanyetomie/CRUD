d3c50b1c697700e0beb15492363b7ba4190b576c
adekanyetomie <adekanyetomy@gmail.com>
2020-06-03 09:45:21 +0100

    fixed patch request issue

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask,request, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, make_response
 from toy import Toy
-from flask_modus import Modus
+from flask_modus import Modus #note that url_decode should be mported from werkzeug.url
 
 
 
@@ -33,28 +33,34 @@ toys = [lego, rubi, cube]
 def index():
     if request.method == 'POST':
         toys.append(Toy(request.form['name']))
-        return redirect(url_for('index'))     
+        return redirect(url_for('index'))  
+        
     return render_template('index.html', toys=toys)
 
 @app.route('/toys/new')
 def new():
     return render_template('new.html')
 
-@app.route('/toys/<int:id>' ,methods = ['GET', 'PATCH'])
+@app.route('/toys/<int:id>' ,methods = ['GET', 'PATCH', 'DELETE'])
 def show(id):
 
-    for toy in toys:
-        if toy.id == id:    
-            found_toy = toy
+   # for toy in toys:
+        #if toy.id == id:    
+            #found_toy = toy
+    found_toy = next(toy for toy in toys if toy.id == id)
+    if request.method == b'PATCH':
+        found_toy.name = request.form['name']
+        return redirect(url_for('index')) 
 
-            if request.method == b'PATCH':
-                found_toy.name = request.form['name']
-                return redirect(url_for('index')) 
+    if request.method == b'DELETE':
+        toys.remove(found_toy)
+        return redirect(url_for('index'))    
     return render_template ('show.html', toy= found_toy)
 
 @app.route('/toys/<int:id>/edit')
 def edit(id):
     # refactored found_toy in show() using a generator
     found_toy = next(toy for toy in toys if toy.id == id)
+    
     return render_template('edit.html', toy = found_toy)
 
